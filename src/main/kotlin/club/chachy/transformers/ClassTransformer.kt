8477e092998c3fd7250540701b9a6395cc41eebd
ChachyDev <shalomadecoolboy@outlook.com>
2020-12-23 14:14:21 +0000

    Some documentation
    Signed-off-by: ChachyDev <shalomadecoolboy@outlook.com>

--- src/main/kotlin/club/chachy/transformers/ClassTransformer.kt
+++ src/main/kotlin/club/chachy/transformers/ClassTransformer.kt
@@ -7,9 +7,17 @@ import org.objectweb.asm.ClassReader
 import org.objectweb.asm.ClassWriter
 import org.objectweb.asm.tree.ClassNode
 
-
 open class ClassTransformer : IClassTransformer {
+    // ArrayListMultiMap to hold transformer data
     private val transformers: ArrayListMultimap<String, TransformerBuilder.() -> Unit> = ArrayListMultimap.create()
+
+    /**
+     * Transform a given class.
+     *
+     * @param name Name of class
+     * @param transformedName Transformed name of class
+     * @param bytes Bytes of class
+     */
 
     override fun transform(name: String?, transformedName: String?, bytes: ByteArray?): ByteArray? {
         if (bytes == null) return null
@@ -32,6 +40,13 @@ open class ClassTransformer : IClassTransformer {
             node.accept(it)
         }.toByteArray()
     }
+
+    /**
+     * Load transformer block into a list mapped to the class name.
+     *
+     * @param name Name of the class to be transformed
+     * @param transformer The transformer block where your transformations are.
+     */
 
     fun transform(vararg name: String, transformer: TransformerBuilder.() -> Unit) {
         name.forEach {
--- src/main/kotlin/club/chachy/transformers/dsl/TransformerBuilder.kt
+++ src/main/kotlin/club/chachy/transformers/dsl/TransformerBuilder.kt
@@ -5,19 +5,37 @@ import net.minecraftforge.fml.common.asm.transformers.deobf.FMLDeobfuscatingRema
 import org.objectweb.asm.ClassReader
 import org.objectweb.asm.tree.*
 
+/**
+ * Builder which contains utilities.
+ *
+ * @param classNode ClassNode instance of the class being transformed.
+ *
+ * @param name Transformed name of the class being currently transformed.
+ *
+ * @author ChachyDev
+ * @since 1.0
+ */
+
 class TransformerBuilder(val classNode: ClassNode, val name: String?) {
+    // Get a method from the transforming class' ClassNode
     fun method(name: String) = method(classNode, name)
 
+    // Get a method from a specified class node with its name
     fun method(classNode: ClassNode, name: String) = classNode.methods.firstOrNull { it.name == name }
 
+    // Get a method from a boolean check from the transforming class' ClassNode
     fun method(node: (MethodNode) -> Boolean) = method(classNode, node)
 
+    // Get a method from a specified class node with a boolean check
     fun method(classNode: ClassNode, node: (MethodNode) -> Boolean) = classNode.methods.firstOrNull(node)
 
+    // Get field from a the transforming class' ClassNode
     fun field(name: String) = field(classNode, name)
 
+    // Get a field from a specified ClassNode with a name.
     fun field(classNode: ClassNode, name: String) = classNode.fields.firstOrNull { it.name == name }
 
+    // Create a ClassNode instance from a class name.
     fun clazz(name: String): ClassNode {
         return ClassNode().also {
             ClassReader(name).also { reader ->
@@ -26,6 +44,9 @@ class TransformerBuilder(val classNode: ClassNode, val name: String?) {
         }
     }
 
+    /**
+     * Find an instruction located in a method with a [predicate].
+     */
     inline fun <reified T : AbstractInsnNode> MethodNode.findInstruction(predicate: (T) -> Boolean): T? {
         for (insn in instructions) {
             if (insn is T && predicate(insn)) return insn
@@ -54,7 +75,6 @@ class TransformerBuilder(val classNode: ClassNode, val name: String?) {
                 .instructions
         )
     }
-
 
     fun mapMethodName(classNode: ClassNode, methodNode: MethodNode): String =
         FMLDeobfuscatingRemapper.INSTANCE.mapMethodName(classNode.name, methodNode.name, methodNode.desc)
