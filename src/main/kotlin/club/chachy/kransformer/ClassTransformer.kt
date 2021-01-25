b0705c16cf4f19e97018855d33ec437ee04f136b
dreamhopping <71222289+dreamhopping@users.noreply.github.com>
2021-01-25 22:21:31 +0000

    Start working on implementing debug output

--- src/main/kotlin/club/chachy/kransformer/ClassTransformer.kt
+++ src/main/kotlin/club/chachy/kransformer/ClassTransformer.kt
@@ -1,17 +1,18 @@
 package club.chachy.kransformer
 
 import club.chachy.kransformer.dsl.TransformerBuilder
+import club.chachy.kransformer.handler.WriterHandler
 import com.google.common.collect.ArrayListMultimap
 import net.minecraft.launchwrapper.IClassTransformer
 import org.objectweb.asm.ClassReader
 import org.objectweb.asm.ClassWriter
 import org.objectweb.asm.tree.ClassNode
+import java.io.File
 
 open class ClassTransformer : IClassTransformer {
     // ArrayListMultiMap to hold transformer data
     private val transformers: ArrayListMultimap<String, TransformerBuilder.() -> Unit> = ArrayListMultimap.create()
-
-    private val writerHandlers = mutableListOf<(ClassWriter) -> Unit>()
+    private val writerHandlers = mutableListOf<(WriterHandler) -> Unit>()
 
     /**
      * Transform a given class.
@@ -20,12 +21,10 @@ open class ClassTransformer : IClassTransformer {
      * @param transformedName Transformed name of class
      * @param bytes Bytes of class
      */
-
     override fun transform(name: String?, transformedName: String?, bytes: ByteArray?): ByteArray? {
-        if (bytes == null) return null
+        if (bytes == null || name == null || transformedName == null) return null
 
         val clazzTransformers = transformers[transformedName] ?: return null
-
         if (clazzTransformers.isEmpty()) return bytes
 
         val node = ClassNode()
@@ -42,13 +41,8 @@ open class ClassTransformer : IClassTransformer {
             node.accept(it)
         }
 
-        writerHandlers.forEach { it.invoke(writer) }
-
+        writerHandlers.forEach { it.invoke(WriterHandler(transformedName, writer)) }
         return writer.toByteArray()
-    }
-
-    fun writerHandler(block: (ClassWriter) -> Unit) {
-        writerHandlers.add(block)
     }
 
     /**
@@ -57,10 +51,41 @@ open class ClassTransformer : IClassTransformer {
      * @param name Name of the class to be transformed
      * @param transformer The transformer block where your transformations are.
      */
-
     fun transform(vararg name: String, transformer: TransformerBuilder.() -> Unit) {
         name.forEach {
             transformers.put(it, transformer)
+        }
+    }
+
+    /**
+     * Called after the class writing happens, this can be used like below to write .class files to disk
+     */
+    fun writerHandler(block: (WriterHandler) -> Unit) = writerHandlers.add(block)
+
+    init {
+        writerHandler {
+            val shouldOutputBytecode = System.getProperty("debugBytecode", "false")?.toBoolean() ?: false
+            if (shouldOutputBytecode) {
+                val node = ClassNode()
+                try {
+                    node.accept(it.writer)
+                } catch (t: Throwable) {
+                    System.err.println("Exception when transforming " + node.name + " : " + t.javaClass.simpleName)
+                    t.printStackTrace()
+                }
+
+                val bytecodeDirectory = File("bytecode")
+                bytecodeDirectory.mkdirs()
+
+                val transformedClassName = "${it.transformedName.replace('.', '/')}.class"
+                val bytecodeOutput = File(bytecodeDirectory, transformedClassName)
+
+                try {
+                    bytecodeOutput.writeBytes(it.writer.toByteArray())
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
         }
     }
 }
---
+++ src/main/kotlin/club/chachy/kransformer/handler/WriterHandler.kt
@@ -0,0 +1,6 @@
+package club.chachy.kransformer.handler
+
+import org.objectweb.asm.ClassWriter
+
+class WriterHandler(val transformedName: String, val writer: ClassWriter) {
+}
